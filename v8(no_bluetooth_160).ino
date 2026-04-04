09ea05662d3d7e3ae750e30bf22b0e98655c3def
Jason <lijunyi1126@gmail.com>
2026-04-04 14:15:11 +0800

    clean up

---
+++ v8(no_bluetooth_160).ino
@@ -0,0 +1,162 @@
+//馬達
+#include <ArxContainer.h>
+
+int PWMA = 10;
+int PWMB = 11;
+int AIN2 = 6;
+int AIN1 = 7;
+int BIN2 = 9;
+int BIN1 = 8;
+//紅外線 rd1 最右邊
+#define rd1 A3
+#define rd2 A4
+#define rd3 A5
+#define rd4 A6
+#define rd5 A7
+int I =1;
+std::vector<char> v{'f','l','f','b','f','r','f','b'};
+void setup() {
+  // 馬達
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+
+  // 紅外線
+  pinMode(rd1, INPUT);
+  pinMode(rd2, INPUT);
+  pinMode(rd3, INPUT);
+  pinMode(rd4, INPUT);
+  pinMode(rd5, INPUT);
+
+  //開始
+ 
+  forward(160);
+}
+
+
+
+void loop() {
+  
+  if((analogRead(rd1)>50 || analogRead(rd2)>50)&&(analogRead(rd4)>50 || analogRead(rd5)>50)){
+    forward(160);
+  }else if(analogRead(rd1)>50 || analogRead(rd2)>50){
+    rightward(160);
+  }else if(analogRead(rd5)>50 || analogRead(rd4)>50){
+    leftward(160);
+  }else{
+    forward(160);
+  }
+  if(analogRead(rd4)>100 && analogRead(rd3)>100 && analogRead(rd2)>100){
+    delay(30);
+    if(v[0]=='f'){
+	    forward(160);
+	    delay(140);
+	    leftward(150);
+	    delay(70);
+	    forward(160);
+	    delay(140);
+    }else if(v[0]=='r'){
+	    rightturn(160);
+    }else if(v[0]=='l'){
+	    leftturn(160);
+    }else if(v[0]=='b'){
+	    backturn(80);
+    }
+    v.push_back(v[0]);
+    v.erase(v.begin());
+  }
+  if(analogRead(rd1)<30 && analogRead(rd2)<30&&analogRead(rd3)<30&&analogRead(rd4)<30 && analogRead(rd5)<30){
+    delay(50);
+    if(analogRead(rd1)<30 && analogRead(rd2)<30&&analogRead(rd3)<30&&analogRead(rd4)<30 && analogRead(rd5)<30){
+      backward(70);
+      while(true){
+        if(analogRead(rd1)>50){
+          I=1;
+          break;
+        }
+        if(analogRead(rd5)>50){
+          I=2;
+          break;
+        }
+      }
+      delay(100);
+      if(I==1){
+        while(analogRead(rd3)<50&&(analogRead(rd2)>50||analogRead(rd4)>50)){
+          rightward(160);
+        }
+      }else{
+        while(analogRead(rd3)<50&&(analogRead(rd2)>50||analogRead(rd4)>50)){
+          leftward(160);
+        }
+      }
+    }
+  }
+    
+
+}
+
+void forward(int speed){ //A左輪
+  analogWrite(PWMA, speed/1.05);
+  analogWrite(PWMB, speed);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  digitalWrite(BIN2, LOW);
+  digitalWrite(BIN1, HIGH);}
+void backward(int speed){
+  analogWrite(PWMA, speed/1.05);
+  analogWrite(PWMB, speed);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, HIGH);}
+void leftward(int speed){ //A左輪
+  analogWrite(PWMA, speed/1.57);
+  analogWrite(PWMB, speed);
+  digitalWrite(BIN2, LOW);
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);}
+void rightward(int speed){ //A左輪
+  analogWrite(PWMA, speed/1.05);
+  analogWrite(PWMB, speed/1.5);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  digitalWrite(BIN2, LOW);
+  digitalWrite(BIN1, HIGH);}
+void stop(int speed){ //A左輪
+  analogWrite(PWMA, speed/1.05);
+  analogWrite(PWMB, speed);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(BIN2, LOW);
+  digitalWrite(BIN1, LOW);
+  delay(600);}
+void leftturn(int speed){ //A左輪
+  analogWrite(PWMA, speed/4.20);
+  analogWrite(PWMB, speed);
+  digitalWrite(BIN2, LOW);
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  while(analogRead(rd5)>100){}
+  delay(520);}
+void rightturn(int speed){ //A左輪
+  analogWrite(PWMA, speed/1.05);
+  analogWrite(PWMB, speed/4);
+  digitalWrite(BIN2, LOW);
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  while(analogRead(rd1)>100){}
+  delay(420);}
+void backturn(int speed){ //A左輪
+  analogWrite(PWMA, speed/1.05);
+  analogWrite(PWMB, speed);
+  digitalWrite(BIN2, HIGH);
+  digitalWrite(BIN1, LOW);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  while(analogRead(rd1)>100){}
+  delay(50);
+  while(analogRead(rd1)<100){}}
